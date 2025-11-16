a63decabe5ee72a755a513dc031c7050201ad14c
Jason <Jason@JASONS-PC>
2025-11-16 15:19:02 +0000

    got to Task 5.

--- MyArrayProject/Program.cs
+++ MyArrayProject/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[] alphabet = new char[26];
+int num = 97;
+for(int i = 0; i < alphabet.Length; i++)
+{
+    Console.WriteLine((char)(num));
+    num++; 
+}
+int newNum = 122;
+Console.WriteLine();
+for (int i = 0; i < alphabet.Length; i++)
+{
+    Console.WriteLine((char)(newNum));
+    newNum--;
+}
+
+int uppercase = 67;
+int value;
+for (int i = 0; i < alphabet.Length; i++)
+{
+     
+}
