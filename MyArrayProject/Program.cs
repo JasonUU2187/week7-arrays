5d46c32dbd3e1974a6813005624a92b03978950c
b00981831 <hutton-j10@ulster.ac.uk>
2025-11-17 16:17:22 +0000

    Challenge 2.

--- MyArrayProject/Program.cs
+++ MyArrayProject/Program.cs
@@ -13,9 +13,36 @@ for (int i = 0; i < alphabet.Length; i++)
     newNum--;
 }
 
-int uppercase = 67;
-int value;
+int uppercase = 65;
+int[] value = new int[26];
+Console.WriteLine();
 for (int i = 0; i < alphabet.Length; i++)
 {
-     
+    value[i] = uppercase;
+    Console.WriteLine((char)uppercase);
+    uppercase++;
+}
+
+int[] fibonacci = {13, 1, 2, 3, 5, 8, 1, 34, 21, 55};
+
+Array.Sort(fibonacci); 
+foreach(int i in fibonacci)
+{
+    Console.WriteLine(i);
+}
+
+int numToFind = 34;
+int index;
+for(int i = 0;i < fibonacci.Length;i++)
+{
+    if (fibonacci[i] == numToFind)
+    {
+        index = i;
+        Console.WriteLine(index);
+        break;
+    } else
+    {
+        index = -1;
+    }
+    
 }
---
+++ MyListProject/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> months = new List<string>();
+months.Add("January");
+months.Add("February");
+months.Add("March");
+months.Add("April");
+months.Add("May");
+months.Add("June");
+months.Add("July");
+months.Add("August");
+months.Add("October");
+months.Add("December");
+
+foreach (string month in months)
+{
+    Console.WriteLine(month);
+}
+
+string[] strings = months.ToArray();
+
+Console.WriteLine(strings);
